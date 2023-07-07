91aeabceff2c80ec995189285e5b8d00e2588a0e
Pornnapa-Panyadee <pornnapa.van@gmail.com>
2023-07-07 22:24:21 +0700

    test

--- resources/views/test_pdf02.blade.php
+++ resources/views/test_pdf02.blade.php
@@ -128,19 +128,66 @@
      <div class="pcoded-content">
         <table>
             <tr>
-                <td width="15%"><img src="{{ asset('images/icon/egat.jpg') }}" width="100%"></td>
+                <td width="15%">
+                    <!-- <img src="{{ asset('images/icon/egat.jpg') }}" width="100%"> -->
+                </td>
                 <td width="70%">
                     <div class="text1"> แบบฟอร์มการตรวจสภาพฝาย</div>
                     <div class="text2">โครงการพัฒนาระบบสารสนเทศการตรวจประเมินสภาพฝายและวางแผนปรับปรุงเพิ่มประสิทธิภาพฝาย
                         <br>ในพื้นที่ลุ่มน้ำแม่จาง จังหวัดลำปาง
                          <br>โดยการไฟฟ้าฝ่ายผลิตแห่งประเทศไทย (กฟผ) แม่เมาะ ร่วมกับมหาวิทยาลัยเชียงใหม่ </div>
                 </td>
-                <td width="15%"><img src="{{ asset('images/icon/cmu.png') }}" width="60%" ></td>
+                <td width="15%">
+                    <!-- <img src="{{ asset('images/icon/cmu.png') }}" width="60%" > -->
+                </td>
             </tr>
             <tr>
                 <td colspan="3" class="text2">**************************************************************************************************************</td>
             </tr>
         </table>
+        <?php 
+            $code=str_split($weir[0]->weir_code );
+        ?>
+        <div class="text4" >
+            <table>
+              <tr>
+                <td> รหัสหมู่บ้าน 
+                    <font class="box" >0</font>
+                    <font class="box">0</font>
+                    <font class="box">0</font>
+                    <font class="box">0</font>
+                    <font class="box">0</font>
+                    <font class="box">{{$code[7]}}</font>
+                    <font class="box">{{$code[8]}}</font>
+                    <font class="box">{{$code[9]}}</font>
+                </td>
+                <td> รหัสตำบล 
+                    <font class="box">0</font>
+                    <font class="box">0</font>
+                    <font class="box">0</font>
+                    <font class="box">0</font>
+                    <font class="box">0</font>
+                    <font class="box">0</font>
+                    <font class="box">{{$code[5]}}</font>
+                    <font class="box">{{$code[6]}}</font>
+                </td>
+                <td> รหัสอำเภอ 
+                    <font class="box">0</font>
+                    <font class="box">0</font>
+                    <font class="box">0</font>
+                    <font class="box">0</font>
+                    <font class="box">{{$code[3]}}</font>
+                    <font class="box">{{$code[4]}}</font>
+                </td>
+                <td> รหัสจังหวัด 
+                    <font class="box">0</font>
+                    <font class="box">0</font>
+                    <font class="box">5</font> 
+                    <font class="box">7</font>
+                </td>
+              </tr>
+            </table>
+        </div>
     </div>
     </body>
 </html>
