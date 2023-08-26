833ef803b942779f2f4a8babe4b4ffe7003868df
Pornnapa-Panyadee <pornnapa.van@gmail.com>
2023-08-26 20:40:07 +0700

    test

--- resources/views/reportPDF/reportSum.php
+++ resources/views/reportPDF/reportSum.php
@@ -346,9 +346,9 @@
                             </td>
                         </tr>
                         <tr style="background-color:#DFDFDF">
-                            <td width="40%">1. ส่วนป้องกันเหนือน้ำ : <?php echo (checkpixhas(count($photo1),$photo1[0]["file"],$damage[0])); ?> </td>
+                            <td width="40%">3. ส่วนควบคุมน้ำ : <?php echo (checkpixhas(count($photo1),$photo1[0]["file"],$damage[0])); ?> </td>
                             <td style="text-align:center;" width="10%"><?php echo $sediment['check1']; ?></td>
-                            <td width="40%">2. ส่วนเหนือน้ำ   : <?php echo (checkpixhas(count($photo2),$photo2[0]["file"],$damage[1])); ?></td>
+                            <td width="40%">4. ส่วนท้ายน้ำ   : <?php echo (checkpixhas(count($photo2),$photo2[0]["file"],$damage[1])); ?></td>
                             <td style="text-align:center;" width="10%"><?php echo $sediment['check2']; ?></td>
                         </tr>
                         <tr>
@@ -370,9 +370,9 @@
                             </td>
                         </tr>
                         <tr style="background-color:#DFDFDF">
-                            <td width="40%">1. ส่วนป้องกันเหนือน้ำ : <?php echo (checkpixhas(count($photo1),$photo1[0]["file"],$damage[0])); ?> </td>
+                            <td width="40%">5. ส่วนป้องกันท้ายน้ำ : <?php echo (checkpixhas(count($photo1),$photo1[0]["file"],$damage[0])); ?> </td>
                             <td style="text-align:center;" width="10%"><?php echo $sediment['check1']; ?></td>
-                            <td width="40%">2. ส่วนเหนือน้ำ   : <?php echo (checkpixhas(count($photo2),$photo2[0]["file"],$damage[1])); ?></td>
+                            <td width="40%">6. ระบบส่งน้ำ   : <?php echo (checkpixhas(count($photo2),$photo2[0]["file"],$damage[1])); ?></td>
                             <td style="text-align:center;" width="10%"><?php echo $sediment['check2']; ?></td>
                         </tr>
                         <tr>
